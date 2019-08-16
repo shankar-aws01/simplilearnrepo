be30e08dbd839242ac2894d1ad188239c495c070
Shankar <shankar.aws01@gmail.com>
2019-08-16 22:09:40 +0530

    Maven project commit

---
+++ simplilearn27-master/src/main/java/com/boraji/tutorial/servlet/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package com.boraji.tutorial.servlet;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author imssbora , this code is modified by srikanth for child branch
+   I am writing something related to child branch
+  
+ */ 
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+   private static final long serialVersionUID = 1L;
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+         throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example. new version");
+   }
+}
